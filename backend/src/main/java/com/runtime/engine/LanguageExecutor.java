dc4a14cf3814c958b26ef134c2ab5c4c063d7aab
lazyfetch <237130889+lazyfetch@users.noreply.github.com>
2026-03-04 21:21:23 +0530

    cleanup

--- backend/src/main/java/com/runtime/engine/LanguageExecutor.java
+++
@@ -1,9 +0,0 @@
-package com.runtime.engine;
-
-import com.runtime.model.ApiResponse;
-import com.runtime.model.ExecutionResult;
-
-public interface LanguageExecutor
-{
-    ApiResponse<ExecutionResult> execute(String code,String stdin);
-}
